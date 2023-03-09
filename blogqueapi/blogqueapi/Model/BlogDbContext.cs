b1f0db737254027c2cbe55281d0736dd7628b115
Dhruvi Sherathiya <83078196+DhruviSherathiya@users.noreply.github.com>
2023-03-09 09:52:56 +0530

    API added

---
+++ blogqueapi/blogqueapi/Model/BlogDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace blogqueapi.Model
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+
+        
+
+    }
+}

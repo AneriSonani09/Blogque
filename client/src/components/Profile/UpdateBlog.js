7839fa1e1df539fcb3f31227a08b39fff9812e2e
Dhruvi Sherathiya <83078196+DhruviSherathiya@users.noreply.github.com>
2023-03-27 23:19:16 +0530

    Update bloag added

--- client/src/components/Profile/UpdateBlog.js
+++ client/src/components/Profile/UpdateBlog.js
@@ -1,48 +1,106 @@
 import React, { useEffect, useState } from 'react'
-import Link from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from 'axios';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function UpdateBlog() {
-    const tokenstr = localStorage.getItem("usertoken")
-    const str = tokenstr.split("/")
-    const token = str[0]
-    const [blog, setBlog] = useState({
-        id: "",
-        title: "",
-        content: "",
-        UserId:""
-    })
+  const navigate = useNavigate()
+  const tokenstr = localStorage.getItem("usertoken")
+  const str = tokenstr.split("/")
+  const token = str[0]
+  const [blog, setBlog] = useState({
+    title: "",
+    content: "",
+    imageURL: "",
+    publishDate: "",
+    category: "",
+    authorId: ""
+  })
 
-    //code for geeting blogid
-    const path = window.location.pathname
-    const array = path.split("/")
-    const id = array[2]
+  //code for geeting blogid
+  const path = window.location.pathname
+  const array = path.split("/")
+  const id = array[2]
 
-    const setValue = ({currentTarget: input}) => {
-        setBlog({...blog, [input.name]:input.value})
-    }
+  const setValue = ({ currentTarget: input }) => {
+    setBlog({ ...blog, [input.name]: input.value })
+  };
 
-
-    const getData = async () => {
+  const getData = async () => {
     try {
-    const res = await axios.get(`https://localhost:7079/api/Blogs/${id}`, {
-      headers: {
-        "Content-Type": "application/json"
-      }
-    });
-    setBlog(res.data);
-  } catch (error) {
-    console.error(error);
-  }
-};
+      const res = await axios.get(`https://localhost:7079/api/BlogPosts/${id}`, {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+      setBlog(res.data);
+      console.log("Blog to be updated")
+      console.log(res.data)
+      console.log(blog)
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    useEffect(() => {
-        getData()
+  console.log("================================================")
+  const updateBlog = async () => {
+    try {
+      const res = await axios.put(`https://localhost:7079/api/BlogPosts/${id}`, blog, {
+        withCredentials: true,
+        headers: {
+          'accept': 'text/plain',
+          'Content-Type': 'application/json',
+          'Authorization': `bearer ${token}`
+        }
+      });
+      console.log(res.data);
+      toast.success('Blog updated successfully!');
+      navigate('/')
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  useEffect(() => {
+    getData()
   }, [])
 
   return (
     <div>
-      <h1>Update </h1>
+      <form class="container" onSubmit={updateBlog}>
+        <h3 className='card-title'>Update a Blog</h3> <br />
+        <div className="mb-3">
+          <label>Blog Title</label>
+          <input type="text"
+            className="form-control"
+            name="title"
+            onChange={setValue}
+            value={blog.title} 
+            />
+        </div>
+        <div className="mb-3">
+          <label>Blog Category</label>
+          <input type="text"
+            className="form-control"
+            onChange={setValue}
+            name="category"
+            value={blog.category} />
+        </div>
+        <div className="mb-3">
+          <label>Tell your story ... </label>
+          <input type="textarea"
+            className="form-control"
+            onChange={setValue}
+            name="content"
+            value={blog.content} />
+        </div>
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            Create
+          </button>
+        </div>
+      </form>
+      <ToastContainer />
     </div>
   )
 }
